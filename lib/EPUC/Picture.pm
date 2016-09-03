672a03ff87e6d9f20c34194f14a661f766e66118
Eric Wolf <coyocanid@gmail.com>
2016-09-03 12:00:35 -0700

    eat poop u cat in yote

---
+++ lib/EPUC/Picture.pm
@@ -0,0 +1,59 @@
+package EPUC::Picture;
+
+use strict;
+
+use Yote::Server;
+
+use base 'Yote::ServerObj';
+
+use File::Copy;
+use File::Path 'make_path';
+use UUID::Tiny;
+
+sub url {
+    my( $self, $size ) = @_;
+
+    my $image = $self->get_image;
+    print STDERR Data::Dumper->Dump([$image,$self->{DATA},"?? ($self)"]);
+    my $ext   = $image->get_file_extension;
+    my $sizes = $image->get_sizes;
+    if( @$sizes ) {
+        my( $found_size ) =  grep { $_ eq $size } @$sizes;
+        $found_size ||= $sizes->[0];
+        return $image->get_base_url . "_$found_size.$ext";
+    }
+    return $image->get_base_url . ".$ext";
+} #url
+
+sub develop {
+    my( $self, $image, @sizes ) = @_;
+
+    $self->set_image( $image );
+    
+    my $orig_file_path = $image->get_file_path;
+    my $ext            = $image->get_file_extension;
+    
+    my( @path ) = split( /-/, UUID::Tiny::create_uuid_as_string() );
+
+    my $filename      = pop @path;
+    my $relative_path = join( '/', @path );
+
+    my $file_root_path = "/var/www/html/epuc/images/$relative_path";
+    my $file_path      = "$file_root_path/$filename";
+    make_path( $file_root_path );
+
+    $image->set_sizes( [@sizes] );
+    if( @sizes ) {
+        for my $size (@sizes) {
+            `convert $orig_file_path -resize $size ${file_path}_$size.$ext`;
+        }
+    } else {
+        copy( $image->get_file_path, $file_path ) or die "$@ $!";
+    }
+
+    $image->set_base_url( "/epuc/images/$relative_path/$filename" );
+    print STDERR Data::Dumper->Dump([$self->{DATA},"DEVELOP ($self)"]);
+    $self;
+} #develop
+
+1;
