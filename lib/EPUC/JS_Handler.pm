4a915af8fa370aaf30cf0f984e3e9be73bd4c3e7
ewolf <coyocanid@gmail.com>
2016-11-29 17:09:48 -0800

    adding javascript layer atop kudos and upload icon

---
+++ lib/EPUC/JS_Handler.pm
@@ -0,0 +1,30 @@
+package EPUC::JS_Handler;
+
+
+use strict;
+
+use Apache2::Request;
+use Apache2::RequestRec;
+use Apache2::RequestIO;
+use Apache2::Upload;
+use Apache2::Const qw(:common);
+use APR::Request::Param;
+use APR::Request::Apache2;
+use Data::Dumper;
+use URI::Escape;
+
+use EPUC::Operator;
+
+sub handler {
+    print STDERR Data::Dumper->Dump(["JSHANDLER"]);
+    my $r = Apache2::Request->new( shift );
+
+    my $operator = new EPUC::Operator(
+        template_path => '/opt/yote/templates',
+        );
+    return $operator->handle_json_request( $r );
+} #handler
+
+
+
+1;
